fadef0a4a93ce7a38840c86c3111ffd48199a077
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-01-11 22:45:43 +0530

    Update divisedbyn.py

--- Projects-Practices/Intermediate-python-Basics/divisedbyn.py
+++ Projects-Practices/Intermediate-python-Basics/divisedbyn.py
@@ -5,12 +5,13 @@
 
 i = 1
 while i<=100:
-    if (i%3 and i%5 == 0):
+    if (i%3==0 and i%5 == 0):
         print(i, "FizzBuzz")
+    elif i%3==0:
+        print(i, "Fizz")
+    elif i%5 == 0:
+        print(i, "Buzz")
     else:
-        if i%3==0:
-            print(i, "Fizz")
-        elif i%5 == 0:
-            print(i, "Buzz")
+        print(i)
     i+=1
     
